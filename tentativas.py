cb3d25806727d2ead8d4edb91d47d8a2d25da4c4
fabirms <Frn#2032>
2022-10-04 09:33:12 -0400

    Adicionando a biblioteca random

--- tentativas.py
+++ tentativas.py
@@ -1,31 +1,35 @@
+import random
 
 print("---------------------------------")
 print("Bem vindo ao jogo de Adivinhação!")
 print("--------------------------------")
 
-numero_secreto = 42
-total_de_tentativas = 3
-rodada = 1
 
-while (rodada <= total_de_tentativas):
-    chute_str = input("Digite o seu número: ")
+numero_secreto = random.randrange(1,101)
+total_de_tentativas = 3
+
+for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    print("Você digitou: ", chute_str)
+
+    chute_str = input("Digite um número entre 1 e 100: ")
+    print("Você digitou " , chute_str)
     chute = int(chute_str)
 
-    acertou = numero_secreto == chute
-    maior = chute > numero_secreto
-    menor = chute < numero_secreto
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
-    if (acertou):
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
+
+    if(acertou):
         print("Você acertou!")
+        break
     else:
-        if (maior):
-            print("Você errou! O seu chute foi maior que o número secreto.")
-        elif (menor):
-            print("Você errou! O seu chute foi menor que o número secreto.")
-
-    rodada = rodada + 1
-
+        if(maior):
+            print("Você errou! O seu chute foi maior do que o número secreto.")
+        elif(menor):
+            print("Você errou! O seu chute foi menor do que o número secreto.")
 
 print("Fim do jogo")
